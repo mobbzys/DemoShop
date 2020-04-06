c97a9ce00e8adf24a63305637d177809e2f95ba8
DantheMobbs <daniel.s.mobbs@gmail.com>
2020-04-06 19:40:45 +0100

    Init Project

---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en-US">
+  <head>
+    <meta charset="utf-8">
+    <title>JavaScript Learning Platform</title>
+    <link rel="stylesheet" type="text/css" href="Asset/Css/style.css">
+    <script>
+    	//Code Goes Here
+    </script>
+
+  </head>
+ <body>
+  	<div>
+  		<!--Header-->	
+  		<header id="header">
+  			<nav class="top-nav">
+  				<div class="container clearfix">
+	  				<a href="index.html" class="logo">Template Website</a>
+	  				<ul class="nav">
+		  				<li><a class="active" href="index.html">Home</a></li>
+		  				<li><a href="#">About</a></li>
+		  				<li><a href="#">Portfolio</a></li>
+		  				<li><a href="#">Contact</a></li>
+		  			</ul>
+		  		</div>			  		
+	  		</nav>
+	  		<div class="main-head">
+				<div class="container clearfix">
+					<div class="award"><a href="#"><img src="Images/logo.png" alt="I like cheese" width="158"></a></div>
+					<div class="logo-second"><a href="#"><img src="Images/flowerlogo.png" width="218"></a></div>
+					<div class="opening-times"><a href="#"><img class="img-award" src="Images/Award.png" alt="I like cheese" width="189"></a></div>
+				</div>	
+				<nav class="main-nav">
+					<div class="container">
+						<ul class="main-nav-left">
+							<li><a href="#">Nursery & Plants</a></li>
+	  						<li><a href="#">Home & Garden</a></li>
+						</ul>
+						<ul class="main-nav-right">
+							<li><a href="#">Cafe & Food</a></li>
+	  						<li><a href="#">Garden Club</a></li>
+	  						<li><a href="#">Events</a></li>
+						</ul>					
+					</div>
+				</nav>	
+			</div>	  			
+		</header> 
+
+		<!--Nav-->
+	
+
+		<!--Banner-->
+		<section>
+			<div class="clearfix">
+				<img class="mandp" src="Images/village.jpg" width="100%">		
+			</div>	
+		</section>
+
+
+		<!--<main>			
+			<div class="inner-container">
+
+		  		<h1>JavaLearning Platform!</h1> 		
+		    		
+				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.</p>
+
+			    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.</p>
+
+			</div>
+		</main>-->
+
+		</div>
+
+		<footer>
+			
+
+		</footer> 
+
+	</div>	
+</body>
+</html>
